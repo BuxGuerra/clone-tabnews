22bf937735192a87e95917552d23374aa2aa93eb
BuxGuerra <guilhermebuxguerra@gmail.com>
2025-12-17 12:52:30 -0300

    corrige msg

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 function Home() {
     return <h1>Boa tarde meu amigo João, espero que você esteja bem em terras baianas.
-        Fiz o deploy dessa página na Vercel. Como é só algo pessoa, dá pra usar a versão grátis.
+
+        Fiz o deploy dessa página na Vercel. Como é só algo pessoal, dá pra usar a versão grátis.
+
         Eles automatizam todo o CI/CD e o deploy, ou seja, a única coisa que fiz foi subir o código no GitHub e eles atualizam o site automaticamente.
         O que não entendi ainda é se eles lidam com front, back e DB ou só front. Te atualizo mais pra frente.
+        
         Um abraço do novo rei do tênis!
     </h1>
 }
