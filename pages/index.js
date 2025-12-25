756a063e3f16f68cb29137bf51ea430e08de7cf5
BuxGuerra <guilhermebuxguerra@gmail.com>
2025-12-25 16:33:26 -0300

    adiciona scripts lint:check e lint:fix e corrrige os formatações

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 function Home() {
-    return <h1>Boa tarde meu amigo João, espero que você esteja bem em terras baianas.
-
-        Fiz o deploy dessa página na Vercel. Como é só algo pessoal, dá pra usar a versão grátis.
-
-        Eles automatizam todo o CI/CD e o deploy, ou seja, a única coisa que fiz foi subir o código no GitHub e eles atualizam o site automaticamente.
-        O que não entendi ainda é se eles lidam com front, back e DB ou só front. Te atualizo mais pra frente.
-        
-        Um abraço do novo rei do tênis!
-    </h1>
+    return (
+        <h1>
+            Boa tarde meu amigo João, espero que você esteja bem em terras
+            baianas. Fiz o deploy dessa página na Vercel. Como é só algo
+            pessoal, dá pra usar a versão grátis. Eles automatizam todo o CI/CD
+            e o deploy, ou seja, a única coisa que fiz foi subir o código no
+            GitHub e eles atualizam o site automaticamente. O que não entendi
+            ainda é se eles lidam com front, back e DB ou só front. Te atualizo
+            mais pra frente. Um abraço do novo rei do tênis!
+        </h1>
+    );
 }
 
 export default Home;
